70e5f26c1d32944a5c15edf4c7ca55d1a3d25cea
OM-GIDEON <mensahg2023@outlook.com>
2023-08-12 12:33:44 -0400

    data cleaning script created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # US_DATASCIENCE_SALARY_PROJ
+## Dataset obtained from
+https://www.kaggle.com/datasets/venkatamanibabu/data-science-jobs-data-in-the-united-states
--- data_collection.py
+++ data_collection.py
@@ -1,5 +1,10 @@
+
 import pandas as pd
 
 path = "C:/Users/Dell/Documents/US_DATASCIENCE_SALARY_PROJ/US_Data_Science_Jobs.xlsx"
 
-df = pd.read_excel(path)
+
+import pandas as pd
+
+path = "C:/Users/Dell/Documents/US_DATASCIENCE_SALARY_PROJ/US_Data_Science_Jobs.xlsx"
+
